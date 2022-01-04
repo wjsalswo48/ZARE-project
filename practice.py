348cc085578d203140eb5323218a3b88e1b528bb
MINJAE JEON <wjsalswo985@naver.com>
2022-01-04 17:29:51 +0900

    freshman first commit

--- practice.py
+++ practice.py
@@ -4,4 +4,8 @@ station = "신도림"
 print(station + "행 열차가 들어오고 있습니다.")
 station = "인천공항"
 print(station + "행 열차가 들어오고 있습니다.")
+station = "김해공항"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "제주공항"
+print(station + "행 열차가 들어오고 있습니다.")
 #24분 42초
